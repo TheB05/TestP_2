cbb094b6af52d3847b7bfe5ff6582f244c73a566
TheB05 <branderbaez02@gmail.com>
2024-11-02 19:59:00 -0400

    Creating the forms.

--- obj/Debug/net8.0/TestP_2.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestP_2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestP_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fba4faaf628a5285e814a8ac42d75056c54364bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1104d9a1ab63798a53144fb8c3a6555dea293dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestP_2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestP_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
